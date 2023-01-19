39b689b82f32e62f6fbecaa4a5bbda645d0ebeb6
Angel Corral <82479794+angelcorral2021@users.noreply.github.com>
2023-01-18 22:08:50 -0300

    first commit

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,13 @@
+//rfce
+
+
+
+const ListadoPacientes = () => {
+  return (
+    <div className="w-1/2"><h2 className="font-black text-3xl text-center">
+        Formulario
+        </h2></div>
+  )
+}
+
+export default ListadoPacientes
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.jsx"],//Le indicamos a tailwind los archivos que queremos aplicar sus estilos
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
